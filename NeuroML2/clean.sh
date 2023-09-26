5bb3882b3c14b9632c06d045cf0cf6036ab8415d
pgleeson <p.gleeson@gmail.com>
2023-09-26 10:46:29 +0100

    Test on sbml

--- NeuroML2/clean.sh
+++ NeuroML2/clean.sh
@@ -1,3 +1,3 @@
 mkdir ~/temp/dump
-mv -v report*txt *.pdf *.dat *.spikes *.gdf *.pkl *.hoc *.mod  *~ *.log *nrn.py NET_*nml *brian.py *brian2.py *nest.py *pynn.py *mdf.* *definition.py  *netpyne.py  *moose.py  *.nestml *.png *.ini *.svg *.pov *.gv *cell.json *synapse.json *main.json tmp* *csv *mdf.json *mdf.yaml *.mdf *eden.py *__lems.xml *nml ~/temp/dump
+mv -v report*txt *.pdf *.dat *.spikes *.gdf *.pkl *.hoc *.mod  *~ *.log *nrn.py NET_*nml *brian.py *brian2.py *nest.py *pynn.py *mdf.* *definition.py  *netpyne.py  *moose.py *.sedml *.sbml *.cellml  *.nestml *.png *.ini *.svg *.pov *.gv *cell.json *synapse.json *main.json tmp* *csv *mdf.json *mdf.yaml *.mdf *eden.py *__lems.xml  ~/temp/dump
 rm -rf x86_64 i386 umac arm64  *.pyc *.c *.so  __pycache__ *~ .*~
---
+++ NeuroML2/test_tellurium.py
@@ -0,0 +1,30 @@
+
+import tellurium as te
+te.setDefaultPlottingEngine('matplotlib')
+
+import sys
+
+sedml_file='LEMS_Regular_HindmarshRose.sedml'
+if len(sys.argv)==2:
+    sedml_file=sys.argv[1]
+
+# For technical reasons, any software which uses libSEDML
+# must provide a custom build - Tellurium uses tesedml
+try:
+    import libsedml
+except ImportError:
+    import tesedml as libsedml
+sedml_doc = libsedml.readSedML(sedml_file)
+n_errors = sedml_doc.getErrorLog().getNumFailsWithSeverity(libsedml.LIBSEDML_SEV_ERROR)
+print('Read SED-ML file %s, number of errors: %i'%(sedml_file,n_errors))
+if n_errors > 0:
+    print(sedml_doc.getErrorLog().toString())
+    
+print(sedml_doc)
+
+print(dir(sedml_doc))
+print(sedml_doc.toSed())
+
+
+# execute SED-ML using Tellurium
+te.executeSEDML(sedml_doc.toSed(), workingDir='.')
